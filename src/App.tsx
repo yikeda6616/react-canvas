18ed219ee552098460c45ade05e9ffc953fd3b21
Yasushi Ikeda <yikeda6616@gmail.com>
2021-10-30 16:32:17 +0900

    refactor: Shorten codes

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,28 @@ import React, { useEffect, useState } from "react";
 
 function App() {
   const [png, setPng] = useState<string | null>(null);
-  const width = 255;
-  const height = 255;
 
   useEffect(() => {
     const canvas = document.createElement("canvas");
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = 250;
+    canvas.height = 250;
     const ctx = canvas && canvas.getContext("2d");
     if (!canvas || !ctx) return;
 
     // canvas描画処理
-    ctx.clearRect(0, 0, width, height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#bada55";
-    ctx.fillRect(0, 0, width, height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     setPng(canvas.toDataURL());
   }, []);
 
   return (
-    <div>
+    <>
       <h1>React.js と CanvasでPNG生成</h1>
       <h2>Canvasを頑張って描く場合</h2>
-      {png && (
-        <div className="comp" style={{ display: "flex" }}>
-          <img alt="icon" src={png} />
-        </div>
-      )}
-    </div>
+      {png && <img alt="icon" src={png} />}
+    </>
   );
 }
 
