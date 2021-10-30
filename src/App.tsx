82c5416e98c1ddb7fe7a1cb97a5a2f907bfce032
Yasushi Ikeda <yikeda6616@gmail.com>
2021-10-30 16:18:34 +0900

    feat: Add canvas sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [png, setPng] = useState<string | null>(null);
+  const width = 255;
+  const height = 255;
+
+  useEffect(() => {
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const ctx = canvas && canvas.getContext("2d");
+    if (!canvas || !ctx) return;
+
+    // canvas描画処理
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = "#bada55";
+    ctx.fillRect(0, 0, width, height);
+
+    setPng(canvas.toDataURL());
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React.js と CanvasでPNG生成</h1>
+      <h2>Canvasを頑張って描く場合</h2>
+      {png && (
+        <div className="comp" style={{ display: "flex" }}>
+          <img alt="icon" src={png} />
+        </div>
+      )}
     </div>
   );
 }
