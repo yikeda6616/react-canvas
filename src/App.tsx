0b633dbc8c9a80c42f5cfe9b4c9392034bb4ea4d
Yasushi Ikeda <yikeda6616@gmail.com>
2021-11-12 18:54:05 +0900

    feat: Add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import React, { useEffect, useState } from "react";
 import html2canvas from "html2canvas";
+import styled from "styled-components";
+
+const StyledTargetComponent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #5957c4;
+  color: #fff;
+  border-radius: 50% 20% / 10% 40%;
+  width: 150px;
+  height: 150px;
+  margin-bottom: 10px;
+  p {
+    text-align: center;
+  }
+`;
 
 const TargetComponent: React.FC = () => (
-  <div id="canvas">画像になるコンポーネント</div>
+  <StyledTargetComponent id="canvas">
+    <p>※画像はイメージです</p>
+  </StyledTargetComponent>
 );
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  div.image {
+    width: 150px;
+    height: 150px;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
 
 function App() {
   const [png, setPng] = useState<string | null>(null);
@@ -17,11 +49,17 @@ function App() {
   }, []);
 
   return (
-    <>
+    <Wrapper>
       <h1>React.js html2canvasで画像生成</h1>
       <TargetComponent />
-      {png && <img alt="canvas" src={png} />}
-    </>
+      <p>↑React Component</p>
+      <p>↓PNG画像</p>
+      {png && (
+        <div className="image">
+          <img alt="canvas" src={png} />
+        </div>
+      )}
+    </Wrapper>
   );
 }
 
